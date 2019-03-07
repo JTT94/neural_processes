154fbb2921e004fdc6ba707884a308aa2857d694
aaaaana <36134434+aaaaana@users.noreply.github.com>
2019-03-07 13:48:10 +0000

    Updating quadratics

--- experiments/Ana/convex_fns.py
+++ experiments/Ana/convex_fns.py
@@ -8,10 +8,15 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
-params = NeuralProcessParams(dim_r=2, dim_z=2, n_hidden_units_h=[8], n_hidden_units_g=[32, 32, 32])
+
+#=========================================================
+# INITIALISATION
+
+# Initial setup
+params = NeuralProcessParams(dim_r=2, dim_z=2, n_hidden_units_h=[64, 64], n_hidden_units_g=[64, 64, 64],
+                             noise_std=0.05)
 
 tf.reset_default_graph()
-
 sess = tf.Session()
 
 # Placeholders for training inputs
@@ -28,23 +33,38 @@ train_op, loss = init_neural_process(x_context, y_context, x_target, y_target,
 init = tf.global_variables_initializer()
 sess.run(init)
 
-n_iter = 100000
+n_iter = 500000
 loss_freq = n_iter/10
+n_obs = 100
+q_low = 0.001
+q_high = 20
+p_low = -20
+p_high = 20
+x_range_low = -5
+x_range_high = 5
+q_pred = 10
+p_pred = 2
+
+#=========================================================
+# TRAINING
 
 train_xs = []
 train_q = []
 train_ys = []
+train_p = []
+
 for i in range(n_iter):
-    n_obs = 20
-    xs = np.random.uniform(-3, 3, n_obs)
-    q = random.uniform(0.001, 20)
-    ys = q * xs**2
+    xs = np.random.uniform(x_range_low, x_range_high, n_obs)
+    q = random.uniform(q_low, q_high)
+    p = random.uniform(p_low, p_high)
+    ys = q * (xs)**2 + p
 
     train_xs.append(xs)
     train_q.append(q)
+    train_p.append(p)
     train_ys.append(ys)
 
-    n_context = random.choice(range(1, 11))
+    n_context = random.choice(range(50,70))
     feed_dict = split_context_target(xs.reshape(-1, 1), ys.reshape(-1, 1), n_context, x_context, y_context, x_target,
                                      y_target)
     a = sess.run((train_op, loss), feed_dict=feed_dict)
@@ -52,11 +72,13 @@ for i in range(n_iter):
         print("Loss: {:.3f}".format(a[1]))
 
 
-q = 2
-xs = np.random.uniform(-3, 3, 5)
-ys = q * xs**2
-x_star = np.linspace(-3, 3, 100)
-y_star = q * x_star**2
+#=========================================================
+# PLOTTING PREDICTIONS
+
+xs = np.random.uniform(x_range_low, x_range_high, 5)
+ys = q_pred * (xs)**2 + p_pred
+x_star = np.linspace(x_range_low, x_range_high, 100)
+y_star = q_pred * (x_star)**2 + p_pred
 
 def plot_prediction(ax, xs, ys, x_star, y_star, plot_true = True, xlim = (-4.5, 4.5), ylim=(-1.5, 1.5), sess= tf.get_default_session()):
     posterior_predict_op = posterior_predict(
@@ -75,8 +97,8 @@ def plot_prediction(ax, xs, ys, x_star, y_star, plot_true = True, xlim = (-4.5, 
     ax.set_ylim(ylim)
 
 fig, axes = plt.subplots(3, 1, figsize=(5,5))
-xss = [np.array(xs) for xs in [[0], [0, 1], [-2, -1, 0, 1, 2]]]
-yss = [q * xs**2 for xs in xss]
+xss = [np.array(xs) for xs in [[3], [1, 3], [-2, -1, 0, 1, 2]]]
+yss = [q_pred * (xs)**2 + p_pred for xs in xss]
 plot_true = False
 ylim=(-30, 30)
 for ax, xs, ys in zip(axes, xss, yss):
